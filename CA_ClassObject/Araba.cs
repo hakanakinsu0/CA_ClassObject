068445569108e8d622a3fc6ef38b7db582f0d3c0
HAKAN AKINSU <hakanakinsu@outlook.com>
2024-07-28 17:03:30 +0300

    Class ve Obje Ornegi

---
+++ CA_ClassObject/Araba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_ClassObject
+{
+    public class Araba
+    {
+        //Ozellikler
+        public string Marka;
+        public string Model;
+        public int Yil;
+
+        //Davranislar
+        public void Calistir()
+        {
+            Console.WriteLine("Araba calisti.");
+        }
+
+    }
+}
--- CA_ClassObject/Program.cs
+++ CA_ClassObject/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CA_ClassObject;
+
+Araba araba1 = new Araba();
+araba1.Marka = "Toyota";
+araba1.Model = "Corolla";
+araba1.Yil = 2021;
+
+araba1.Calistir();
